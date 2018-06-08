3327e1ed8b14c74465668ac6ee7d08164b67c9c7
Joan Miquel Espada Sabat <joanmi@espada.cat>
2018-06-08 14:07:47 +0200

    main files

--- src/createdb.js
+++ src/createdb.js
@@ -1,15 +1,16 @@
 import path from 'path'
-import mongo from 'mongodb'
 import Immutable from 'immutable'
 import fs from 'fs'
-const dbname = 'blockchain'
-const urlmongo =  'mongodb://localhost:27017'
-const dbcollection = 'transactions'
+import {collectionTrans, collectionUsers } from './constants'
+
 
 class CreateDB
 { 
+    constructor(database)
+    {
+        this.database= database
+    }
     
-
     async importFile(fileToBeImported)
     {
         var contents = fs.readFileSync(fileToBeImported, 'utf8');
@@ -17,44 +18,53 @@ class CreateDB
         return data
     }
 
-    async storeInDB(documents)
-    {
-        let client, database
-        try{
-            client = await mongo.MongoClient.connect(urlmongo)
-            database = await client.db(dbname)
-
-            const collection = database.collection(dbcollection)
-
-            collection.remove() 
-            
-            for(let i=0;i<documents.size;i++)
-            {
-                const aux = await documents.get(i)
-                collection.insert({_id: i, doc: aux})
-                 
-            }
-
-        }catch(err) {
-            console.log(err)
-        }finally{
-            
-
-            client.close()
+    async storeInDB(collection, documents)
+    {       
+        for(let i=0;i<documents.size;i++)
+        {
+            documents.get(i).then( (data) => collection.insertOne({_id: i, data}))        
         }
     }
 
-    start(filesToRead)
+    storeUsers(collection, users)
     {
-        const files = new Immutable.List(filesToRead)
+        for(let i=0; i<users.size;i++){
+            const user = users.get(i)
+            collection.insert( {_id:i, name: user.name, address: user.address, totalAmount:0} )  
+        }
+    }
+
+    loadFiles(filesToRead)
+    {
+        
         const docs= []
-        files.map( x=>{     
+        filesToRead.map( x=>{     
             const doc = this.importFile( path.resolve(__dirname,'..','transactions', x))
             docs.push(doc)
         })
-        const docsToProcess = new Immutable.fromJS(docs)
-        this.storeInDB(docsToProcess)
-        
+        return new Immutable.fromJS(docs)
+    }
+
+    async loadMainData(filesToRead, users)
+    {
+    
+        try{
+            
+            const docsToProcess = this.loadFiles(filesToRead)
+            
+            let collection1 = this.database.collection(collectionTrans)
+            collection1.remove() 
+
+            await this.storeInDB(collection1, docsToProcess)
+
+            let collection2 = this.database.collection(collectionUsers)
+            collection2.remove() 
+
+            this.storeUsers(collection2, users)
+            
+        }catch(err) {
+            console.log(err) //eslint-disable-line 
+        }
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,49 @@
+import mongo from 'mongodb'
+import Immutable from 'immutable'
 import CreateDB from './createdb'
+import Sum from './sum'
+import {urlmongo,dbname} from './constants'
 
-const data = new CreateDB()
 
-data.start(['transactions-1.json','transactions-2.json'])
-//data.start(['transactions-1.json'])
+const files = ['transactions-1.json',
+                'transactions-2.json']
+const listOfusers = Immutable.List([
+    {name:'Wesley Crusher', address:'mvd6qFeVkqH6MNAS2Y2cLifbdaX5XUkbZJ'},  
+    {name:'Leonard McCoy', address:'mmFFG4jqAtw9MoCC88hw5FNfreQWuEHADp'},
+    {name:'Jonathan Archer', address:'mzzg8fvHXydKs8j9D2a8t7KpSXpGgAnk4n'},
+    {name:'Jadzia Dax', address:'2N1SP7r92ZZJvYKG2oNtzPwYnzw62up7mTo'},
+    {name:'Montgomery Scott', address:'mutrAf4usv3HKNdpLwVD4ow2oLArL6Rez8'},
+    {name:'James T. Kirk', address:'miTHhiX3iFhVnAEecLjybxvV5g8mKYTtnM'},
+    {name:'Spock', address:'mvcyJMiAcSXKAEsQxbW9TYZ369rsMG6rVV'}
+])
+
+mongo.MongoClient.connect(urlmongo).then( client =>{
+
+    const database = client.db(dbname)
+
+    const data = new CreateDB(database)
+
+    data.loadMainData(files,listOfusers).then(()=>{
+        
+        const operate = new Sum(database)
+
+        operate.start()
+            .then(()=> {
+                //client.close()
+            } )
+            .catch( err=> { 
+                console.log(err) //eslint-disable-line 
+                client.close()
+            })
+
+    }).catch( err=> {
+        console.log(err)//eslint-disable-line 
+        client.close()
+    })
+}).catch( err=> console.log(err))//eslint-disable-line 
+            
+
+
+
+
+
